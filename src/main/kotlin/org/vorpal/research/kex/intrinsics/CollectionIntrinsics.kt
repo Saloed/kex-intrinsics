6e60f0a861dccd573596f484a93f57a0aa438946
Saloed <napster_1997@mail.ru>
2022-09-22 18:20:44 +0300

    more array intrinsics

--- src/main/kotlin/org/vorpal/research/kex/intrinsics/CollectionIntrinsics.kt
+++ src/main/kotlin/org/vorpal/research/kex/intrinsics/CollectionIntrinsics.kt
@@ -165,6 +165,230 @@ object CollectionIntrinsics {
     }
 
     @JvmStatic
+    fun arrayCopyAndGrow(src: DoubleArray, growSize: Int): DoubleArray {
+        return src.copyOf(src.size + growSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndTrim(src: DoubleArray, trimSize: Int): DoubleArray {
+        return src.copyOf(src.size - trimSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftRight(src: DoubleArray, shiftStartIndex: Int, shiftSize: Int): DoubleArray {
+        val result = arrayCopyAndGrow(src, shiftSize)
+        for (i in shiftStartIndex until src.size) {
+            result[i + shiftSize] = src[i]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftLeft(src: DoubleArray, shiftStartIndex: Int, shiftSize: Int): DoubleArray {
+        val result = arrayCopyAndTrim(src, shiftSize)
+        for (i in shiftStartIndex until result.size) {
+            result[i] = src[i + shiftSize]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndGrow(src: FloatArray, growSize: Int): FloatArray {
+        return src.copyOf(src.size + growSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndTrim(src: FloatArray, trimSize: Int): FloatArray {
+        return src.copyOf(src.size - trimSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftRight(src: FloatArray, shiftStartIndex: Int, shiftSize: Int): FloatArray {
+        val result = arrayCopyAndGrow(src, shiftSize)
+        for (i in shiftStartIndex until src.size) {
+            result[i + shiftSize] = src[i]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftLeft(src: FloatArray, shiftStartIndex: Int, shiftSize: Int): FloatArray {
+        val result = arrayCopyAndTrim(src, shiftSize)
+        for (i in shiftStartIndex until result.size) {
+            result[i] = src[i + shiftSize]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndGrow(src: LongArray, growSize: Int): LongArray {
+        return src.copyOf(src.size + growSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndTrim(src: LongArray, trimSize: Int): LongArray {
+        return src.copyOf(src.size - trimSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftRight(src: LongArray, shiftStartIndex: Int, shiftSize: Int): LongArray {
+        val result = arrayCopyAndGrow(src, shiftSize)
+        for (i in shiftStartIndex until src.size) {
+            result[i + shiftSize] = src[i]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftLeft(src: LongArray, shiftStartIndex: Int, shiftSize: Int): LongArray {
+        val result = arrayCopyAndTrim(src, shiftSize)
+        for (i in shiftStartIndex until result.size) {
+            result[i] = src[i + shiftSize]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndGrow(src: IntArray, growSize: Int): IntArray {
+        return src.copyOf(src.size + growSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndTrim(src: IntArray, trimSize: Int): IntArray {
+        return src.copyOf(src.size - trimSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftRight(src: IntArray, shiftStartIndex: Int, shiftSize: Int): IntArray {
+        val result = arrayCopyAndGrow(src, shiftSize)
+        for (i in shiftStartIndex until src.size) {
+            result[i + shiftSize] = src[i]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftLeft(src: IntArray, shiftStartIndex: Int, shiftSize: Int): IntArray {
+        val result = arrayCopyAndTrim(src, shiftSize)
+        for (i in shiftStartIndex until result.size) {
+            result[i] = src[i + shiftSize]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndGrow(src: ShortArray, growSize: Int): ShortArray {
+        return src.copyOf(src.size + growSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndTrim(src: ShortArray, trimSize: Int): ShortArray {
+        return src.copyOf(src.size - trimSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftRight(src: ShortArray, shiftStartIndex: Int, shiftSize: Int): ShortArray {
+        val result = arrayCopyAndGrow(src, shiftSize)
+        for (i in shiftStartIndex until src.size) {
+            result[i + shiftSize] = src[i]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftLeft(src: ShortArray, shiftStartIndex: Int, shiftSize: Int): ShortArray {
+        val result = arrayCopyAndTrim(src, shiftSize)
+        for (i in shiftStartIndex until result.size) {
+            result[i] = src[i + shiftSize]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndGrow(src: CharArray, growSize: Int): CharArray {
+        return src.copyOf(src.size + growSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndTrim(src: CharArray, trimSize: Int): CharArray {
+        return src.copyOf(src.size - trimSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftRight(src: CharArray, shiftStartIndex: Int, shiftSize: Int): CharArray {
+        val result = arrayCopyAndGrow(src, shiftSize)
+        for (i in shiftStartIndex until src.size) {
+            result[i + shiftSize] = src[i]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftLeft(src: CharArray, shiftStartIndex: Int, shiftSize: Int): CharArray {
+        val result = arrayCopyAndTrim(src, shiftSize)
+        for (i in shiftStartIndex until result.size) {
+            result[i] = src[i + shiftSize]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndGrow(src: BooleanArray, growSize: Int): BooleanArray {
+        return src.copyOf(src.size + growSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndTrim(src: BooleanArray, trimSize: Int): BooleanArray {
+        return src.copyOf(src.size - trimSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftRight(src: BooleanArray, shiftStartIndex: Int, shiftSize: Int): BooleanArray {
+        val result = arrayCopyAndGrow(src, shiftSize)
+        for (i in shiftStartIndex until src.size) {
+            result[i + shiftSize] = src[i]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftLeft(src: BooleanArray, shiftStartIndex: Int, shiftSize: Int): BooleanArray {
+        val result = arrayCopyAndTrim(src, shiftSize)
+        for (i in shiftStartIndex until result.size) {
+            result[i] = src[i + shiftSize]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndGrow(src: ByteArray, growSize: Int): ByteArray {
+        return src.copyOf(src.size + growSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndTrim(src: ByteArray, trimSize: Int): ByteArray {
+        return src.copyOf(src.size - trimSize)
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftRight(src: ByteArray, shiftStartIndex: Int, shiftSize: Int): ByteArray {
+        val result = arrayCopyAndGrow(src, shiftSize)
+        for (i in shiftStartIndex until src.size) {
+            result[i + shiftSize] = src[i]
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun arrayCopyAndShiftLeft(src: ByteArray, shiftStartIndex: Int, shiftSize: Int): ByteArray {
+        val result = arrayCopyAndTrim(src, shiftSize)
+        for (i in shiftStartIndex until result.size) {
+            result[i] = src[i + shiftSize]
+        }
+        return result
+    }
+
+    @JvmStatic
     fun <T> arrayCopyAndGrow(src: Array<T>, growSize: Int): Array<T?> {
         return src.copyOf(src.size + growSize)
     }
@@ -191,4 +415,5 @@ object CollectionIntrinsics {
         }
         return result
     }
+
 }
